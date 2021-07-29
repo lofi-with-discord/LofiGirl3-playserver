f3c0cc321d7ae2d86fd7d1a890c7b4f507aabe3d
Park Min Hyeok <pmhstudio.pmh@gmail.com>
2021-07-29 17:55:33 +0900

    feat: add fallback operator
    Signed-off-by: Park Min Hyeok <pmhstudio.pmh@gmail.com>

--- src/events/onReady.ts
+++ src/events/onReady.ts
@@ -5,22 +5,25 @@ import LavalinkClient from '../structures/LavalinkClient'
 export default function onReady (lavalink: LavalinkClient, db: DatabaseClient) {
   return async function (client: BotClient) {
     const guilds = client.guilds.cache.filter((g) => !!g.voice)
-    for (const [, guild] of guilds) {
-      if (!guild.voice?.channel) continue
 
-      const voiceChannel = guild.voice.channel
-      const membersIn = voiceChannel.members.filter((m) => !m.user.bot).size
+    lavalink.on('ready', async () => {
+      for (const [, guild] of guilds) {
+        if (!guild.voice?.channel) continue
 
-      if (membersIn < 1) {
-        await lavalink.stop(voiceChannel)
-        continue
+        const voiceChannel = guild.voice.channel
+        const membersIn = voiceChannel.members.filter((m) => !m.user.bot).size
+
+        if (membersIn < 1) {
+          await lavalink.stop(voiceChannel)
+          continue
+        }
+
+        const theme = await db.getThemeURL(voiceChannel)
+        const track = await lavalink.getTrack(theme)
+        lavalink.play(voiceChannel, track)
       }
 
-      const theme = await db.getThemeURL(voiceChannel)
-      const track = await lavalink.getTrack(theme)
-      lavalink.play(voiceChannel, track)
-    }
-
-    console.log('ready...')
+      console.log('ready...')
+    })
   }
 }
