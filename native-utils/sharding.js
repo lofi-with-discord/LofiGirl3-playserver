33b0d05cb528cceef482a2a2519195f5b3da0c2a
Park Min Hyeok <pmhstudio.pmh@gmail.com>
2021-09-05 13:46:06 +0900

    chore: add sharding metrics
    Signed-off-by: Park Min Hyeok <pmhstudio.pmh@gmail.com>

--- native-utils/sharding.js
+++ native-utils/sharding.js
@@ -1,7 +1,9 @@
+const io = require('@pm2/io')
 const dotenv = require('dotenv')
 const { ShardingManager } = require('discord.js')
 
 dotenv.config()
+const channelCount = io.metric({ name: 'Realtime User' })
 
 const manager = new ShardingManager('./dist/index.js', {
   token: process.env.DISCORD_TOKEN,
@@ -10,3 +12,14 @@ const manager = new ShardingManager('./dist/index.js', {
 
 manager.on('shardCreate', (shard) => console.log(`Launched shard #${shard.id}`))
 manager.spawn()
+
+setInterval(async () => {
+  const channelCounts =
+    await manager.broadcastEval((c) =>
+      c.guilds.cache.reduce((prev, curr) =>
+        prev + (curr.me?.voice?.channel
+          ? curr.me.voice.channel.members.filter((m) => !m.user.bot).size
+          : 0), 0))
+
+  channelCount.set(channelCounts)
+}, 1000)
